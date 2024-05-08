548dbb66db05f2e651f1dd72cbc1d5f8d8699b3e
Motty Butbul <moti234bent@gmail.com>
2024-05-08 20:48:33 +0300

    style 11

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,6 +3,8 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { languageContext } from "../../App";
 import "./style.css";
 import Language from "../Language";
+import { FaLanguage, FaBackward } from "react-icons/fa";
+
 
 export default function Header() {
   const { language } = useContext(languageContext);
@@ -18,15 +20,15 @@ export default function Header() {
   return (
     <div className="header">
       <button
-        className="deButton relative"
+        className="deButton2 relative"
         onClick={() =>handleClick()}
       >
-        {language === "hebrew" ? "שפה" : "भाषा"}
+        <FaLanguage />
       </button>
       {openMenu && <Language setOpenMenu={setOpenMenu} />}
       {location.pathname !== "/items" && (
-        <button className="deButton" onClick={onClick} value={"../items"}>
-          {language === "hebrew" ? "חזרה לרשימה" : "आदेश"}
+        <button className="deButton3" onClick={onClick} value={"../items"}>
+          {language === "hebrew" ? "חזרה" : "आदेश"}
         </button>
       )}
     </div>
